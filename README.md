cd6e184836450358b3af8d6a7316bbb0b7936f35
heveraldo <heveraldob12@gmail.com>
2023-03-12 21:19:19 -0300

    Spark

---
+++ README.md
@@ -0,0 +1,30 @@
+# Treino de Apache Airflow com a API do Twitter -[Alura](https://www.alura.com.br)
+### Apache Airflow Training with Twitter API 
+
+Descrição:
+
+| :placard: Vitrine.Dev |    Repositório do [Heveraldo](https://www.linkedin.com/in/heveraldo-serra-7b3544200/)  |
+| -------------  | --- |
+| :sparkles: Name        | **Curso de Apache Airflow: extração de dados**
+| :label: Tecnologias | Python,Apache Airflow (tecnologias utilizadas)
+| :rocket: URL         | https://cursos.alura.com.br/course/apache-airflow-extracao-dados
+
+<!-- Inserir imagem com a #vitrinedev ao final do link -->
+<p align="left"> <img src="https://github.com/Heveraldob12/Airflow_twitter/blob/main/Fotos/Screenshot%20from%202023-03-09%2018-46-15.png" alt="heveraldob12"   /> </p>
+
+
+## Detalhes do projeto/Project's details
+
+
+Utilização da API do Twitter para carregar dados e fazer um pipeline com os posts relacionados com a query que eu estabeleci, foi uma extração bem curta,por causa
+da limitação do nivel gratuito da conta de desenvolverdo do Twitter.
+
+Using the Twitter API to load data and make a pipeline with the posts related to a query that I established,it was a pretty short extraction, because of the
+developer's Twitter account free tier limitation.
+
+
+## 
+☢️☢️☢️ Qualquer erro de código ou gramatical, por favor entre em contato
+
+
+☢️☢️☢️ Any code or grammatical errors, please contact me
--- dags/twitter_dags.py
+++ dags/twitter_dags.py
@@ -1,26 +1,48 @@
 import sys
 sys.path.append("../airflow")
 
-
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.models import DAG
 from airflow.utils.dates import days_ago
 from operators.twitter_operator import TwitterOperator
 from datetime import datetime, timedelta
 from os.path import join
+from pathlib import Path
+
+with DAG(dag_id = "TwitterDAG", start_date=days_ago(3), schedule_interval="@daily") as dag:
+    
+    BASE_FOLDER = join("datalake/{stage}/twitter_datascience/{partition}",)
 
 
-with DAG(dag_id = "TwitterDAG", start_date=days_ago(2), schedule_interval="@daily") as dag:
+
+    PARTITION_FOLDER_EXTRACT = "extract_date={{ data_interval_start.strftime('%Y-%m-%d') }}"
+
     TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
     query = "datascience"
 
 
-    to = TwitterOperator(file_path=join("datalake/twitter_datascience",
-                                        "extract_date={{ ds }}",
+    twitter_operator = TwitterOperator(file_path=join(BASE_FOLDER.format(stage="Bronze",partition=PARTITION_FOLDER_EXTRACT),
                                         "datascience_{{ ds_nodash }}.json"),
                                         query=query,
                                         start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
                                         end_time="{{ data_interval_end.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
                                         task_id="twitter_datascience")
     
+    twitter_transform = SparkSubmitOperator(task_id="transform_twitter_datascience",
+                                           application="/home/heveraldo/airflow/src/spark/transformation.py",
+                                           name="twitter_transformation",
+                                           application_args=["--src",BASE_FOLDER.format(stage="Bronze", partition=PARTITION_FOLDER_EXTRACT),
+                                            "--dest", BASE_FOLDER.format(stage="Silver", partition=""),
+                                            "--process-date", "{{ ds }}"])
+    twitter_insight = SparkSubmitOperator(task_id="insight_twitter",
+                                           application="/home/heveraldo/airflow/src/spark/insight_tweet.py",
+                                           name="insight_twitter",
+                                          application_args=["--src", BASE_FOLDER.format(stage="Silver", partition=""),
+                                            "--dest", BASE_FOLDER.format(stage="Gold", partition=""),
+                                            "--process-date", "{{ ds }}"])
+
+
+twitter_operator >> twitter_transform #>> twitter_insight
+
 
 
